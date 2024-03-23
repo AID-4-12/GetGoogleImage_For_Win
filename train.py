8349a17eabeee0c86722d44e554fe03c8442a8d9
imnyang <me@imnyang.xyz>
2024-03-23 10:58:43 +0900

    How to use?

--- train.py
+++ train.py
@@ -4,4 +4,4 @@ from ultralytics import YOLO
 model = YOLO('yolov8s-cls.yaml')
 model = YOLO('yolov8s-cls.pt')
 
-model.train(data='C:/Users/imnyang/Nextcloud/train_dataset/', epochs=20)
+model.train(data='C:/Users/imnyang/Desktop/GetGoogleImage/train_dataset/cat', epochs=20)
